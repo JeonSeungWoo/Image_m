9d8d8bd372bc8ee7613953d79419e0f997b61fb1
21class-007 <aqlsz@naver,com>
2019-10-10 09:24:53 +0900

    insertPage

--- Image_s/src/main/java/org/spring/woo/controller/DragUploadController.java
+++ Image_s/src/main/java/org/spring/woo/controller/DragUploadController.java
@@ -1,15 +1,33 @@
 package org.spring.woo.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spring.woo.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/dragUpload/*")
+@RequestMapping("/dragDrop/*")
 @Controller
 public class DragUploadController {
 	private static final Logger logger = LoggerFactory.getLogger(DragUploadController.class);
 	
 	
-
+	@Inject
+	BoardService service;
+	
+	
+	@RequestMapping(value = "/insertPage")
+	public void insertPage()throws Exception{
+		
+	}
+	
+	
+	
+	
+	
+    
+	
+	
 }
